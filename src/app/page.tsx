8229299a10eab03ee9350e3d56a44d9eb7b070e1
Jonathan Concepcion <JConcepcion@Arcanys.com>
2025-09-28 09:34:46 +0800

    intial push

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-[#E6EEFA] min-h-screen flex flex-col justify-center items-center py-10">
+
+    </main>
+  );
+}
